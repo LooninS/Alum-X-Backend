5b9b2b6bed59d5cae648d4a706ad88ec8dd2b2d9
LooninS <hushed55@proton.me>
2025-12-31 17:02:05 +0530

    Added HouseRepo.java

---
+++ src/main/java/com/opencode/alumxbackend/basics/LooninS/house/repository/HouseRepository.java
@@ -0,0 +1,8 @@
+package com.opencode.alumxbackend.basics.LooninS.house.repository;
+
+import com.opencode.alumxbackend.basics.LooninS.house.House;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HouseRepository extends JpaRepository<House, Long> {
+}
+
